a1e7db7865c663a25c9e00f6a36f5501e6fc0daf
Drakodas <l_mauritz@gmx.de>
2021-05-23 12:53:30 +0200

    +Dropping items after full death

--- src/main/java/io/github/drakodas/hardcoremultiplayer/events/OnDeath.java
+++ src/main/java/io/github/drakodas/hardcoremultiplayer/events/OnDeath.java
@@ -3,12 +3,18 @@ package io.github.drakodas.hardcoremultiplayer.events;
 import io.github.drakodas.hardcoremultiplayer.Grave;
 import io.github.drakodas.hardcoremultiplayer.HardcoreMultiplayer;
 import io.github.drakodas.hardcoremultiplayer.utils.Utils;
-import org.bukkit.GameMode;
-import org.bukkit.Sound;
+import org.bukkit.*;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class OnDeath implements Listener {
 
@@ -27,8 +33,8 @@ public class OnDeath implements Listener {
                 player.sendTitle("§4YOU DIED", "Lives left: " + Utils.getLives(player), 10,35, 10);
                 player.getServer().broadcastMessage(HardcoreMultiplayer.PREFIX + player.getName() + " died! They have " + Utils.getLives(player) + " left.");
                 player.playSound(player.getLocation(), Sound.valueOf(HardcoreMultiplayer.INSTANCE.getConfig().getString("sound_death.sub_death")), 5, 1);
-                event.setCancelled(true);
                 HardcoreMultiplayer.INSTANCE.saveHashmapData();
+                event.setCancelled(true);
             }
             else if (Utils.getLives(player) == 1)
             {
@@ -43,6 +49,12 @@ public class OnDeath implements Listener {
                 event.setCancelled(true);
                 Grave grave = new Grave(player);
                 grave.create();
+                for(ItemStack item: player.getInventory())
+                {
+                    player.getInventory().setItemInMainHand(item);
+                    player.dropItem(true);
+                }
+                player.getInventory().clear();
                 HardcoreMultiplayer.INSTANCE.saveHashmapData();
             }
         }
